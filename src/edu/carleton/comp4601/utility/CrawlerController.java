a6af0d078e7252787d25c158f9f5b528adf4465c
Luke <daschko.l@gmail.com>
2018-04-14 13:19:29 -0400

    monster working! dual site working!

--- src/edu/carleton/comp4601/utility/CrawlerController.java
+++ src/edu/carleton/comp4601/utility/CrawlerController.java
@@ -119,7 +119,7 @@ public class CrawlerController {
         CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
         
        controller.addSeed("https://www.workopolis.com/jobsearch/find-jobs?&st=RELEVANCE&ak=" + searchword + "&l=canada&&pn=1");
-       //controller.addSeed("https://www.monster.ca/jobs/search/?q=" + searchword + "&where=canada");
+       controller.addSeed("https://www.monster.ca/jobs/search/?q=" + searchword + "&where=canada");
        // controller.addSeed("https://www.indeed.ca/jobs?q=" + searchword + "&l=canada");
         
         controller.start(MyCrawler.class, numberOfCrawlers);
@@ -145,7 +145,7 @@ public class CrawlerController {
 		Date startdate = new Date();
 		System.out.println(dateFormat.format(startdate));	
 		CrawlerController cc = new CrawlerController();
-		Map<String, Double> results = cc.getResults("java"); //TODO this is a stub until the actual crawling gets done
+		Map<String, Double> results = cc.getResults("html"); //TODO this is a stub until the actual crawling gets done
 		System.out.println("The top ten skills are:");
 		for(String skill : results.keySet()) {
 			System.out.println("Skill: "+skill+" Percent:"+results.get(skill)*100);
--- src/edu/carleton/comp4601/utility/MyCrawler.java
+++ src/edu/carleton/comp4601/utility/MyCrawler.java
@@ -1,6 +1,7 @@
 package edu.carleton.comp4601.utility;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
@@ -27,6 +28,12 @@ public class MyCrawler extends WebCrawler {
 	
 	boolean repeat = false;
 	static int count = 0;
+	
+	long startCrawlTime = 0;
+	long endCrawlTime   = 0;
+	
+	static int htmlcount = 0;
+	static int jobCount = 0;
 	
 	String searchWord = CrawlerController.crawlSearchWord;
 	
@@ -67,7 +74,7 @@ public class MyCrawler extends WebCrawler {
         	 
         	 /*Monster*/
         	                         //https://www.monster.ca/jobs/search/?q=java&where=canada&page=2
-        	 else if (href.startsWith("https://www.monster.ca/jobs/search/?q=java&where=canada&page=")) {
+        	 else if (href.startsWith("https://www.monster.ca/jobs/search/?q=" + searchWord + "&where=canada&page=")) {
         		 if(detail==true){System.out.println("INITAL HIT");}
         		 String pgnum = href.substring(href.indexOf("page=")+5);
         		 if(detail==true){System.out.println("INITAL HIT: " + pgnum);}
@@ -94,6 +101,14 @@ public class MyCrawler extends WebCrawler {
                 ((href.startsWith("https://www.workopolis.com/jobsearch/find-jobs?ak=" + searchWord + "&l=canada&lg=en&pn=")) ||
                  (href.startsWith("https://www.monster.ca/jobs/search/?q=" + searchWord + "&where=canada&page=")));
      }
+     
+     
+     
+     @Override
+     public boolean shouldFollowLinksIn(WebURL url){
+    	 startCrawlTime = System.currentTimeMillis();
+    	 return true;
+     }
 
      /**
       * This function is called when a page is fetched and ready
@@ -101,6 +116,9 @@ public class MyCrawler extends WebCrawler {
       */
      @Override
      public void visit(Page page) {
+    	 endCrawlTime = (System.currentTimeMillis() - startCrawlTime) * 10;
+    	 this.getMyController().getConfig().setPolitenessDelay(new Long(endCrawlTime).intValue());
+    	 
          String url = page.getWebURL().getURL();
          if(detail==true){System.out.println("Search page URL: " + url);}
 
@@ -133,15 +151,18 @@ public class MyCrawler extends WebCrawler {
     				int endIndex = jobLink.indexOf(" class")-1;
     				jobLink = jobLink.substring(startIndex, endIndex);
     				jobLink = "https://www.workopolis.com" + jobLink;
-    			
     				
-    				
-    		     //Step THREE: Prase for skills (pre job page)
-    				try { htmlParseForSkills(jobLink); } 
-    				catch (Exception e) {
-    					if(detail==true){System.out.println("job link: " + jobLink + " url: " + url);}
-    					if(detail==true){System.out.println("Unable to reach url");}
-    				}				
+    				if(urls.contains(jobLink)){if(detail==true){System.out.println("repeat monster job");}}
+    				else{
+    					urls.add(jobLink);
+    					
+    			 //Step THREE: Prase for skills (pre job page)
+        			    try { htmlParseForSkills(jobLink); }
+        				catch (Exception e) {
+        					if(detail==true){System.out.println("job link: " + jobLink + " url: " + url);}
+        					if(detail==true){System.out.println("Unable to reach url");}
+        				}
+    				}
               	}
               	if(detail==true){System.out.println("jobsperpage: " + jobsPrePage);}
              
@@ -161,37 +182,40 @@ public class MyCrawler extends WebCrawler {
              /*Monster*/
              else if(url.startsWith("https://www.monster.ca")){
             	 //Step ONE: Regex Search for job postings
-
-            	 //@type":"ListItem","position":2,"url":"
-            	 //(type.:.ListItem.,.position.:.,.url.:.*?})
             	 Matcher m = Pattern.compile("(ListItem.\\,.position.\\:.*?\\,.url.\\:.*?\\})").matcher(expression);
             	 
             	 //Step TWO: Connect to each job link (pre search page)
               	 int jobsPrePage = 0;
-              	 while (m.find() && jobsPrePage<26) {
+              	 while (m.find() && jobsPrePage<26) {              		 
               		jobsPrePage++;
-              		
+
                		//trim and polish regex link
     				String jobLink = m.group(1);
     				int startIndex = jobLink.indexOf("url\":\"")+6;
     				int endIndex = jobLink.indexOf("}")-1;
     				jobLink = jobLink.substring(startIndex, endIndex);
     				if(detail==true){System.out.println("monster joblink: " + jobLink);}
-    				urls.add(jobLink);
     				
-    				
-    		     //Step THREE: Prase for skills (pre job page)
-    			    try { htmlParseForSkills(jobLink); }
-    				catch (Exception e) {
-    					if(detail==true){System.out.println("job link: " + jobLink + " url: " + url);}
-    					if(detail==true){System.out.println("Unable to reach url");}
+    				//ensure the link has not been already added.... trust me it happens
+    				if(urls.contains(jobLink)){if(detail==true){System.out.println("repeat monster job");}}
+    				else{
+    					urls.add(jobLink);
+    					
+    			 //Step THREE: Prase for skills (pre job page)
+        			    try { htmlParseForSkills(jobLink); }
+        				catch (Exception e) {
+        					if(detail==true){System.out.println("job link: " + jobLink + " url: " + url);}
+        					if(detail==true){System.out.println("Unable to reach url");}
+        				}
     				}
-    								
+    				jobCount++;				
               	}
+              	//Debugging prints
               	if(detail==true){System.out.println("Total Monster at page " + url + ": " + jobsPrePage);}
               	jobsPrePage = 0; 
+              	if(detail==true){System.out.println("urls set size: " + urls.size() + " totalCount: " + jobCount);}
              }
-         	if(detail==true){System.out.println("urls set size: " + urls.size());}
+         	
           
              
         }
@@ -215,19 +239,26 @@ public class MyCrawler extends WebCrawler {
 		HashMap<String, Boolean> skills = pages.getSkills(linkOfJobPost);
 		
 		 //--look for each skill in page and make a note if it exists
-		String jobExpression = jobPage.html().toString().replace("\n", " ").replace(",", " ").replace(".", " ").replaceAll(";", " ");
+		String jobExpression = jobPage.html().toString();
 		for(String skill: skills.keySet()){
-			if(jobExpression.toLowerCase().indexOf(" " + skill + " ") != -1){
-				//if(detail==true){System.out.println("we gotta skill boy: " + skill);}
+			
+			//These are the only natural language occurences we care about
+			if((jobExpression.toLowerCase().indexOf(" " + skill + " ")  != -1) ||
+			   (jobExpression.toLowerCase().indexOf(" " + skill + ", ") != -1) ||
+			   (jobExpression.toLowerCase().indexOf(" " + skill + ". ") != -1) ||
+			   (jobExpression.toLowerCase().indexOf(" " + skill + "\n") != -1)){
+								
 				Pages.getInstance().addSkill(linkOfJobPost, skill);
+				
+				//debug print
+				if(detail==true){if(skill.equals("html")){htmlcount++;}}
+
 			}
+			
 		}
-		
-		
-		
-		
-    	 
-    	 
-    	 
+		//debug print
+		if(detail==true){System.out.println("html count: " + htmlcount);}
      }
+		
+    
 }
