75b321cbf8b5b11f906dd61db132181141d3c85c
Luke <daschko.l@gmail.com>
2018-04-15 14:11:38 -0400

    database update and changes to crawl + controller

--- src/edu/carleton/comp4601/utility/CrawlerController.java
+++ src/edu/carleton/comp4601/utility/CrawlerController.java
@@ -8,6 +8,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -15,6 +16,9 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 import com.mongodb.BasicDBObject;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
 
 import edu.carleton.comp4601.dao.Equivalencies;
 import edu.carleton.comp4601.dao.Skills;
@@ -70,7 +74,10 @@ public class CrawlerController {
 			if(counts.get(searchTerm)==0) {
 				ratios.put(skill, 0d);
 			} else {
-				ratios.put(skill, (double)counts.get(skill)/(double)counts.get(searchTerm));
+				System.out.println("skill: " + skill);
+				System.out.println("numerator: " + (double)counts.get(skill));
+				System.out.println("denomnatr: " + (double)counts.get(searchTerm) + "\n");
+				ratios.put(skill, (double)counts.get(skill)/  Pages.getInstance().getPages().size());
 			}
 		}
 		//Find the top 10 ratios
@@ -135,10 +142,19 @@ public class CrawlerController {
         RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
         CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
         
-
-        controller.addSeed("https://www.workopolis.com/jobsearch/find-jobs?&st=RELEVANCE&ak=" + searchword + "&l=canada&&pn=1");
-        controller.addSeed("https://www.monster.ca/jobs/search/?q=" + searchword + "&where=canada");
-        controller.addSeed("https://www.jobboom.com/en/job/" + searchword + "_canada/_k-1?dk=" + searchword + "&location=canada&defaultDistance=true");;       
+        
+        
+        if(searchword.equals("c++")){
+        	controller.addSeed("https://www.workopolis.com/jobsearch/find-jobs?&st=RELEVANCE&ak=" + "c%2B%2B" + "&l=canada&&pn=1");
+        	controller.addSeed("https://www.jobboom.com/en/job/" + "c%2B%2B" + "_canada/_k-1?dk=" + "c%2B%2B" + "&location=canada&defaultDistance=true");
+        	//controller.addSeed("https://www.jobboom.com/en/job/c_canada/_k-1?dk=" + searchword + "&location=canada");
+        	controller.addSeed("https://www.monster.ca/jobs/search/?q=" + "c__2B__2B" + "&where=canada");
+        }else{
+        	controller.addSeed("https://www.workopolis.com/jobsearch/find-jobs?&st=RELEVANCE&ak=" + searchword + "&l=canada&&pn=1");
+        //	controller.addSeed("https://www.jobboom.com/en/job/" + searchword + "_canada/_k-1?dk=" + searchword + "&location=canada&defaultDistance=true");
+        	controller.addSeed("https://www.jobboom.com/en/job/c_canada/_k-1?dk=" + searchword + "&location=canada");
+        	controller.addSeed("https://www.monster.ca/jobs/search/?q=" + searchword + "&where=canada");
+        } 
         
         controller.start(MyCrawler.class, numberOfCrawlers);
 	}
@@ -163,123 +179,271 @@ public class CrawlerController {
 		Date startdate = new Date();
 		System.out.println(dateFormat.format(startdate));	
 		CrawlerController cc = new CrawlerController();
-		
+		//cc.getResults("java");
 		//List of skills
 		//
+		
+		
 		List<String> automate = new ArrayList<String>();
-		//automate.add("c++");
-		//automate.add("tomcat");
-		//automate.add("react");
-		//automate.add("ip security");
-		automate.add("kanban");
-		//automate.add("sass");
-		//automate.add("wcf");
-		automate.add("foxpro");
-
-		//gtk
-		//azure
-		//python
-		//d3 js
-		//ip
-		//caffe
-		//dns
-		//objective c
-		//akka
-		//sql server
-		//microstrategy
-		//angular
-		//struts
-		//kafka
-		//handlebar
-		//hive
-		//artifactory
-		//dom
-		//openshift
-		//bootstrap
-		//tensorflow
-		//maf
-		//chart js
-		//jenkins
-		//pl-sql
-		//macos
-		//scipy
-		//sonarqube
-		//jsoup
-		//excel
-		//pandas
-		//pivotal
-		//netconf/yang
-		//php
-		//yarn
-		//cucumber
-		//circleci
-		//vb net
-		//tcp/ip
-		//pig
-		//jms
-		//natural langauge processing
-		//webpack
-		//yocto
-		//apache spark
-		//asp net
-		//j2ee
-		//html
-		//orientdb
-		//etl
-		//adworks
-		//unreal engine
-		//jira
-		//matlab
-		//angular js
-		//maven
-		//android studio
-		//visual studio
-		//maya
-		//google protocol buffers
-		//angularjs
-		//soap
-		//unix
-		//elixir
-		//samza
-		//scala
-		//crystal reports
-		//jade
-		//firebasemessaging
-		//jquery
-		//sql
-		//grunt
-		//qml
-		//rust
-		//unity
-		//git
-		//postgresql
-		//web/db
-		//jpa
-		//mongodb
-		//dhcp
-		//rest
-		//steamworks
-		//chef
-		//jeria
-		//mfc
-		//ruby
-		//puppet
-		//tableau
-		//fraud managment
-		//ceph
-		//subversion
-		//srcum
-		//servlets
-		//lua
-		//qnx
-		//vuejs
-		//xslt
-		//aws
-		//scikit
-		//opencv
+		/*
+		boolean loop = true;
+		DBCollection coll;
+		try {
+			coll = DatabaseSingleton.getInstance().getCollection("skills");
+			DBCursor curs = coll.find();
+			int wordcount = 0;
+			while(loop){
+				String word = "";
+				 if(curs.hasNext()) {
+					 wordcount++;
+					 DBObject o = curs.next();
+		             word = (String) o.get("skill") ; 
+		             automate.add(word);
+		             
+		             if(wordcount == 224/2){
+		            	 loop = false;
+		             }
+				 }
+				
+				
+				
+			}
+		} catch (UnknownHostException e) {
+			
+			e.printStackTrace();
+		}
+		*/
+		/*
+		automate.add("css");
+		
+		//automate.add("html5");
+		
+		automate.add("stata");
+		
+		automate.add("jackrabbit");
+		
+		automate.add("theano");
+		
+		automate.add("ios");
+		
+		automate.add("ajax");
+		
+		automate.add("java");
+		
+	    automate.add("ibm mq");
+		
+		automate.add("travis ci");
+		*/
+		//automate.add("xml");
+		
+		//automate.add("pl/sql");
+		
+		/////////////////////////////////automate.add("xctest");
+		
+		//automate.add("c#");
+		/*
+		automate.add("node js");  
+		
+	automate.add("qt");
+		
+	automate.add("stl");
+		
+		automate.add("mxnet");
+		
+		automate.add("javascript");
+		
+		automate.add("cocoa");
+		
+		automate.add("nosql");
+		
+		automate.add("node");
+		
+		automate.add("cassandra");
+		
+		automate.add("cocoa touch");
+		
+		automate.add("boost");
+		
+		automate.add("agile");
+		*/
+		
+		//automate.add("c#.net");
+		
+		////////////////////////////////automate.add("spacy");
+		/*
+		automate.add("jsf");
+		
+		automate.add("datasynapse");
+		
+		automate.add("websphere");
+		
+		automate.add("tfs");
+		
+		automate.add("restful");
+		
+		automate.add("jsp");
+		
+		automate.add("bcg");
+		
+		automate.add("amazon web services");
+		
+		automate.add("hibernate");
+		
+		automate.add("urbancode");
+		
+		automate.add("d3");
+		*/
+		/*
+		automate.add("ansible");
+		
+		automate.add("wsdl");
+		
+		automate.add("spark");
+		
+		automate.add("waterfall");
+		
+		automate.add("pytorch");
+		
+		automate.add("backbone");
+		
+		automate.add("ejb");
+		
+		automate.add("perforce");
+		
+		automate.add("postgres");
+		
+		automate.add("core data");
+		
+		automate.add("c");
+		
+		automate.add("maxscript");
+		
+		//automate.add("svn");
+		
+		automate.add("docker swarm");
+		
+		automate.add("adobe audience");
+		
+		automate.add("gradle");
+		
+		automate.add("neo4j");
+		
+		automate.add("cobol");
+		
+		automate.add("solace");
+		
+		automate.add("confluence");
+		
+		automate.add("nltk");
+		
+		////////////////////////////////automate.add("core animation");
+		
+		//->automate.add("pl sql");
+		
+		automate.add("jee");
+		*/
+		/*
+		automate.add("bitbucket");
+		
+		//->automate.add("t-sql");
+		
+		automate.add("facebook insights");
+		
+		automate.add("spring");
+		
+		automate.add("XCUI");
+		
+		automate.add("keras");
+		
+		automate.add("corenlp");
+		
+		automate.add("big data");
+		
+		automate.add("swift");
+		
+		automate.add("cron");
+		
+		//automate.add("objective-c");
+		//
+		automate.add("oracle");
+		
+		automate.add("octave");
+		
+		automate.add("cinder");
+		
+		automate.add("xcode");
+		
+		automate.add("wpf");
+		
+		automate.add("kaggle");
+		
+		automate.add("mllib");
+		
+		automate.add("tika");
+		
+		//->automate.add("css3");
+		
+		automate.add("scoop");
+		
+		automate.add("xsd");
+		
+		automate.add("code collaborator");
+		
+		automate.add("jdbc");
+		
+		automate.add("mustache");
+		
+		automate.add("docker");
+		
+		automate.add("jboss");
+		
+		automate.add("uml");
+		
+		automate.add("linux");
+		
+		automate.add("hadooq");
+		
+		automate.add("json");
+		
+		automate.add("iginite");
+		
+		automate.add("sip");
+		
+		automate.add("perl");
+		
+		automate.add("waf");
+		
+		automate.add("ruby on rails");
+		
+		automate.add("junit");
+		
+		automate.add("adobe creative cloud");
+		
+		automate.add("h2o");
+		
+		automate.add("odbc");
+		
+		automate.add("teradata");
+		
+		automate.add("go");
+		
+		automate.add("windows");
+		
+		automate.add("prolog");
+		
+		automate.add("vpn");
+		*/
+		
+		
+		//automate.add("xctest");
+		//automate.add("docker swarm");
+		automate.add("jackrabbit");
+		automate.add("theano");
+		automate.add("ibm mq");
 		
 		for(String searchSk : automate) {
 			Pages.getInstance().reset();
+			MyCrawler.urls = new HashSet<String>();
 			Map<String, Double> results = cc.getResults(searchSk); //TODO this is a stub until the actual crawling gets done
 			System.out.println("Searching: "+searchSk);
 			System.out.println("The top ten skills are:");
@@ -289,5 +453,6 @@ public class CrawlerController {
 			Date date = new Date();
 			System.out.println(dateFormat.format(date));				
 		}
+		
 	}
 }
